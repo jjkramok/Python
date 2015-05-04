4d8a84ebb3e972b63df2a291b636b84ad2b0efeb
Tim Hintzbergen <t.a.h.hintzbergen@student.utwente.nl>
2015-05-04 02:03:57 +0200

    made some custom string stuff

--- main.py
+++ main.py
@@ -1,41 +1,44 @@
 import random
 
-#Bash import statement:
+# Bash import statement:
 #import sys; sys.path.append("/home/tim/git/Python"); import main
 
 class Worker:
-	"""  """
+    """  """
+
     def __init__(self, condition, task):
-        self.condition  = condition
-        self.task       = task
-	self.workspace	= 0
+        self.condition = condition
+        self.task = task
+        self.workspace = 0
+
+    def __str__(self):
+        return str(self.task)
+
     def task(task):
-		if (task == self.task):
-			print("I am already doing: " + task)
-		else:
-			#Chance to start a different task
-			otherTask = random.randint(0,1000) / 1000
-			if (otherTask > 0.80):
-				self.task = task
-				print("I am now doing: " + task)
-				return task
+        if (task == self.task):
+            print("I am already doing: " + task)
+        else:  #Chance to start a different task
+            otherTask = random.randint(0, 1000) / 1000
+        if (otherTask > 0.80):
+            self.task = task
+            print("I am now doing: " + task)
+            return task
+
 
 
 class Mine:
-	def __init__(self):
-		self.assignedWorkers = []		
+    def __init__(self):
+        self.assignedWorkers = []
 
-	def assignWorker(self, worker):
-		self.assignedWorkers += [worker]
-		worker.workspace = self
+    def assignWorker(self, worker):
+        self.assignedWorkers += [worker]
+        worker.workspace = self
+        worker.task = 'mining'
 
-	
 
-d = Worker(1,2);
+d = Worker(1, 2);
 m = Mine();
 m.assignWorker(d);
-			
-		
- 
-			
-        
+for worker in m.assignedWorkers:
+    print(worker)
+
