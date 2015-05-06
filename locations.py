6a75fc45bfd3404555bb8f44724fb5f160828ac4
Tim Hintzbergen <t.a.h.hintzbergen@student.utwente.nl>
2015-05-06 23:57:54 +0200

    added the farm

--- locations.py
+++ locations.py
@@ -4,34 +4,49 @@
 class Location:
     """ """
     def __init__(self):
-        pass
+        self.assignedWorkers = []
 
     def assignWorker(self, worker):
-        if (worker.isFree()):
-            self.assignedWorkers += [worker]
-
-
-
+        pass
 
 
 
 class Mine(Location):
     """ A place were workers mine stone and other minerals """
-    def __init__(self):
-        super(Mine, self).__init__()
-        self.assignedWorkers = []
+    def __init__(self, size):
+        super(Mine).__init__()
+        self.size = size
 
     def assignWorker(self, worker):
-        self.assignedWorkers += [worker]
-        worker.workspace = self
-        worker.task = 'mining'
+        if worker.isFree():
+            self.assignedWorkers += [worker]
+            worker.workspace = self
+            worker.task = 'mining'
 
 
-class Forest(Location):
+class Woods(Location):
     """ """
     def __init__(self, regrowth, size):
-        super(Forest, self).__init__()
+        super(Woods).__init__()
         self.regrowth = regrowth
         self.size = size
 
-    #TODO asigned wordkers
+    def assignWorker(self, worker):
+        if worker.isFree():
+            self.assignedWorkers += [worker]
+            worker.workspace = self
+            worker.task = 'woodcutting'
+
+
+class Farm(Location):
+    """ """
+    def __init__(self, regrowth, size):
+        super(Farm).__init__()
+        self.regrowth = regrowth
+        self.size = size
+
+    def assignWorker(self, worker):
+        if worker.isFree():
+            self.assignedWorkers += [worker]
+            worker.workspace = self
+            worker.task = 'farmer'
