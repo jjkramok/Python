2fa8569127adb67d17f15d218381b0ce803deca1
Tim Hintzbergen <t.a.h.hintzbergen@student.utwente.nl>
2015-05-06 14:34:15 +0200

    super location

--- locations.py
+++ locations.py
@@ -1,11 +1,33 @@
 # All locations/occupations that a worker can inhabit
 
-class Mine:
+
+class Location:
+    """ """
+    def __init__(self):
+        pass
+
+
+
+
+
+
+class Mine(Location):
     """ A place were workers mine stone and other minerals """
     def __init__(self):
+        super(Mine, self).__init__()
         self.assignedWorkers = []
 
     def assignWorker(self, worker):
         self.assignedWorkers += [worker]
         worker.workspace = self
         worker.task = 'mining'
+
+
+class Forest(Location):
+    """ """
+    def __init__(self, regrowth, size):
+        super(Forest, self).__init__()
+        self.regrowth = regrowth
+        self.size = size
+
+    #TODO asigned wordkers
